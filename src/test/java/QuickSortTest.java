f02bc2b0d15ced12c3fe4760a3f695ff7b043572
NikitaPeshko <79310641+NikitaPeshko@users.noreply.github.com>
2021-06-03 13:08:16 +0300

    add test

--- src/test/java/QuickSortTest.java
+++ src/test/java/QuickSortTest.java
@@ -1,12 +1,43 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class QuickSortTest {
+    @Test
+    public void testPartition() throws Exception{
+        QuickSort Partit = new QuickSort();
+        int[] array = { 4, 6, 5, 3, 1, 7, 2,10 };
+        int partActual=Partit.partition(array,0,6);
+        int partExpected=3;
+        assertEquals(partExpected,partActual);
 
-    @org.junit.Test
-    public void partition() {
     }
 
-    @org.junit.Test
-    public void quickSort() {
+    @Test
+    public void testQuickSort() {
+        QuickSort QS = new QuickSort();
+        int[] arrayActual = { 4, 6, 5, 3, 1, 7, 2,10 };
+        QS.quickSort(arrayActual,0,6);
+        int[] arrayExpected={1,2,3,4,5,6,7,10};
+        assertArrayEquals(arrayExpected,arrayActual);
+    }
+
+    @Test
+    public void quickSortString() {
+        QuickSort QS = new QuickSort();
+        String[] StringarrayActual = { "lesha", "nikita", "pasha", "koly", "max", "anton","alex" };
+
+        QS.quickSortString(StringarrayActual,0,6);
+        String[] StringarrayExpected={"alex", "anton", "koly", "lesha", "max", "nikita", "pasha"};
+        assertArrayEquals(StringarrayExpected,StringarrayActual);
+    }
+
+    @Test
+    public void partitionForString() {
+        QuickSort Partit1 = new QuickSort();
+        String[] Stringarray = { "lesha", "nikita", "pasha", "koly", "max", "anton","alex" };
+        int partActual=Partit1.partitionForString(Stringarray,0,6);
+        int partExpected=3;
+        assertEquals(partExpected,partActual);
     }
 }
